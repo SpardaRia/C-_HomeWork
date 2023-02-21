4af909119222579567af1dc7b82f953bc664dc79
Iris <royx0227@gmail.com>
2023-02-21 19:13:40 +0300

    Задачи 41 и 43

---
+++ 6S/Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+вводим через один Enter (разбиваем через Split())
+*/
+
+
+Console.WriteLine("Введдите числа через пробел: ");
+    string s = Console.ReadLine();
+    Console.WriteLine();
+  
+    int count = 0;
+    string[] str =  s.Split(' ');
+   
+       for (int i = 0; i < str.Length; i++)
+    {
+        if(Convert.ToInt32(str[i]) > 0)
+        {
+            count++;
+        }
+    }
+   
+Console.WriteLine(count);
---
+++ 6S/Task43/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int GetNumber(string messege)
+{
+    Console.WriteLine(messege);
+    int resultNumber = 0;
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введены некоректные данные. Повторите попытку.");
+        }
+    }
+
+    return resultNumber;
+}
+
+
+void PointIntersectionStraights(double b1, double b2, double k1, double k2)
+{
+    double x = (b2-b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения прямых: ({x}) и ({y})");
+}
+
+
+double b1 = GetNumber("Введите b1");
+double k1 = GetNumber("Введите k1");
+double b2 = GetNumber("Введите b2");
+double k2 = GetNumber("Введите k2");
+Console.WriteLine();
+PointIntersectionStraights(b1, b2, k1, k2);
