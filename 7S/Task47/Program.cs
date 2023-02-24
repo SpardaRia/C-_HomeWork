2ede5ce6890d7f291ef461d1215047595091db57
Iris <royx0227@gmail.com>
2023-02-24 17:59:10 +0300

    ДЗ к семинару 7

---
+++ 7S/Task47/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+int GetNumber(string messege)
+{
+    Console.WriteLine(messege);
+    int resultNumber = 0;
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введены некоректные данные. Повторите попытку.");
+        }
+    }
+
+    return resultNumber;
+}
+
+double[,] InitMatrix(int rows, int columns)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (10 - (-10)) + (-10), 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+double[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
---
+++ 7S/Task52/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int GetNumber(string messege)
+{
+    Console.WriteLine(messege);
+    int resultNumber = 0;
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введены некоректные данные. Повторите попытку.");
+        }
+    }
+
+    return resultNumber;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+Console.WriteLine();
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ArithmeticMean(int[,] matrix)
+{
+
+    for (int j = 0; j <  matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+           sum = (sum + matrix[i, j]);
+          // Console.WriteLine($"Среднее {sum}");
+        }
+        double arithmeticMean = Math.Round(sum / matrix.GetLength(0), 1, MidpointRounding.ToZero);
+        Console.WriteLine($"Среднее арифметическое в столбце {j+1} равно {arithmeticMean}");
+    }
+
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+
+ArithmeticMean(matrix);
